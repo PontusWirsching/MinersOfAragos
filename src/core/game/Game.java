2ae15137688ee873ff516db3a5efc32cb55288c1
Pontus <flash13records@gmail.com>
2014-09-19 22:57:52 +0200

    First Commit
    Signed-off-by: Pontus <flash13records@gmail.com>

---
+++ src/core/game/Game.java
@@ -0,0 +1,25 @@
+package core.game;
+
+import java.awt.Graphics;
+
+import com.engine.state.State;
+
+public class Game extends State {
+
+	public Game(String name) {
+		super(name);
+	}
+
+	@Override
+	public void update() {
+
+	}
+	
+	@Override
+	public void render(Graphics g) {
+
+	}
+
+
+
+}
---
+++ src/core/game/Start.java
@@ -0,0 +1,33 @@
+package core.game;
+
+import com.engine.LEngine;
+import com.engine.state.StateHandler;
+
+public class Start extends LEngine {
+
+	public Start(int width, int height, String title) {
+		super(width, height, title);
+		
+		StateHandler.addState(new Game("GAME"));
+		
+		StateHandler.setState("GAME");
+		
+		skipIntro();
+		start();
+	}
+
+	@Override
+	public void update() {
+		StateHandler.update();
+	}
+	
+	@Override
+	public void render() {	
+		StateHandler.render(g);
+	}
+
+	public static void main(String[] args) {
+		new Start(1024, 720, "Miners of Aragos");
+	}
+
+}
