c7d07e245c414961a9f63c1a4d6574d3f7baefa9
Pontus <flash13records@gmail.com>
2014-09-23 21:51:27 +0200

    Added proper level support
    Signed-off-by: Pontus <flash13records@gmail.com>

--- src/core/game/Start.java
+++ src/core/game/Start.java
@@ -1,5 +1,11 @@
 package core.game;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+
 import com.engine.LEngine;
 import com.engine.state.StateHandler;
 
@@ -7,16 +13,52 @@ import core.tools.TextureLoader;
 
 public class Start extends LEngine {
 
+	public static final String PATH = System.getenv("APPDATA") + "\\LostSourceStudios\\MinersOfAragos";
+
+	/**
+	 * Will check that game folders exist else create new ones and download latest map files.
+	 */
+	static {
+		File file = new File(PATH);
+
+		if (!file.exists()) {
+			file.mkdirs();
+		}
+
+		File world = new File(PATH + "\\Worlds");
+
+		if (!world.exists()) {
+			world.mkdirs();
+		}
+		try {
+			URL website = new URL("http://www.lostsourcestudios.com/resources/MinersOfAragos/level.tmx");
+			ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+			FileOutputStream fos = new FileOutputStream(PATH + "\\worlds\\" + "level.tmx");
+			fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+		} catch (Exception e) {
+
+		}
+		try {
+			URL website = new URL("http://www.lostsourcestudios.com/resources/MinersOfAragos/tiles.png");
+			ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+			FileOutputStream fos = new FileOutputStream(PATH + "\\worlds\\" + "tiles.png");
+			fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+		} catch (Exception e) {
+
+		}
+
+
+	}
+
 	public Start(int width, int height, String title) {
 		super(width, height, title);
 		TextureLoader.load();
 
 		TextureLoader.load();
-		
+
 		StateHandler.addState(new Game("GAME"));
 		StateHandler.setState("GAME");
-		
-		
+
 		skipIntro();
 		start();
 	}
@@ -25,9 +67,9 @@ public class Start extends LEngine {
 	public void update() {
 		StateHandler.update();
 	}
-	
+
 	@Override
-	public void render() {	
+	public void render() {
 		StateHandler.render(g);
 	}
 
--- src/core/tools/TextureLoader.java
+++ src/core/tools/TextureLoader.java
@@ -6,22 +6,11 @@ public class TextureLoader {
 	
 	public static SpriteSheet inventoryGui;
 	public static SpriteSheet inventoryGuiTemplate;
-	public static SpriteSheet tiles;
 	
 		public static SpriteSheet itemSpriteSheet;
 	
 	public static void load() {
 
-		tiles = new SpriteSheet("/textures/tiles.png");
-		tiles.loadSprite("SPRITE_DIRT", 0, 0, 16, 16);
-		tiles.loadSprite("LEFT_UPPER_CORNER_STONE", 16, 0, 16, 16);
-		tiles.loadSprite("MIDDLE_UPPER_STONE", 32, 0, 16, 16);
-		tiles.loadSprite("LEFT_UPPER_CORNER_STONE", 48, 0, 16, 16);
-		tiles.loadSprite("LEFT_MIDDLE_STONE", 16, 16, 16, 16);
-		tiles.loadSprite("RIGHT_MIDDLE_STONE", 48, 16, 16, 16);
-		tiles.loadSprite("LEFT_LOWER_CORNER_STONE", 16, 32, 16, 16);
-		tiles.loadSprite("LOWER_MIDDLE_STONE", 32, 32, 16, 16);
-		tiles.loadSprite("RIGHT_LOWER_CORNER_STONE", 48, 32, 16, 16);
 		
 		
 		// Loading Sprite Sheets
